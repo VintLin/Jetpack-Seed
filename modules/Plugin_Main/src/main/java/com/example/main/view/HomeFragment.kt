441d0e46116526c1726f82f68490513e220c6db8
VintLin <vintonlin@gmail.com>
2023-02-21 11:10:28 +0800

    update: ShadowView

--- modules/Plugin_Main/src/main/java/com/example/main/view/HomeFragment.kt
+++ modules/Plugin_Main/src/main/java/com/example/main/view/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.main.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import com.example.base.activity.CommonFragment
@@ -8,12 +9,8 @@ import com.example.base.util.once.Once
 import com.example.main.BR
 import com.example.main.R
 import com.example.main.viewmodel.HomeViewModel
-import com.opensource.svgaplayer.SVGACache
-import com.opensource.svgaplayer.SVGAParser
-import com.opensource.svgaplayer.SVGAVideoEntity
+import com.example.main.widget.ShadowUiData
 import kotlinx.android.synthetic.main.fragment_home.*
-import java.net.URL
-
 
 class HomeFragment : CommonFragment() {
     lateinit var mState: HomeViewModel
@@ -28,50 +25,24 @@ class HomeFragment : CommonFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mState.localStatus.postValue(HomeViewModel.LoadState.LOADING)
-    }
-
-    private fun loadLocal() {
-        SVGACache.clearCache()
-        val currentTime = System.currentTimeMillis()
-        // 加载本地动画文件
-        SVGAParser(requireContext()).decodeFromAssets("local.svga", object : SVGAParser.ParseCompletion {
-            override fun onComplete(videoItem: SVGAVideoEntity) {
-                // 解码完成，设置动画数据并播放
-                svga_local.setVideoItem(videoItem)
-                svga_local.startAnimation()
-                mState.localStatus.postValue(HomeViewModel.LoadState.COMPLETE)
-                mState.localTimer.postValue((System.currentTimeMillis() - currentTime) / 1000f)
-            }
-
-            override fun onError() {
-                // 解码失败，进行错误处理
-                mState.localStatus.postValue(HomeViewModel.LoadState.FAIL)
-                mState.localTimer.postValue((System.currentTimeMillis() - currentTime) / 1000f)
-            }
-        })
-    }
-
-    private fun loadNetwork() {
-        SVGACache.clearCache()
-        val currentTime = System.currentTimeMillis()
-        mState.networkStatus.postValue(HomeViewModel.LoadState.LOADING)
-        // 加载网络动画文件
-        SVGAParser(requireContext()).decodeFromURL(URL("https://picfile-baidu.babybus.com/AppConfig/BabyBusFile/20230217/bc83f1dcae8943eaacaed6714d2612e5.svga"), object : SVGAParser.ParseCompletion {
-            override fun onComplete(videoItem: SVGAVideoEntity) {
-                // 解码完成，设置动画数据并播放
-                svga_network.setVideoItem(videoItem)
-                svga_network.startAnimation()
-                mState.networkStatus.postValue(HomeViewModel.LoadState.COMPLETE)
-                mState.networkTimer.postValue((System.currentTimeMillis() - currentTime) / 1000f)
-            }
-
-            override fun onError() {
-                // 解码失败，进行错误处理
-                mState.networkStatus.postValue(HomeViewModel.LoadState.FAIL)
-                mState.networkTimer.postValue((System.currentTimeMillis() - currentTime) / 1000f)
-            }
-        })
+        val ui = ShadowUiData.Config()
+            .setColor(Color.WHITE)
+            .setShadow(
+                outer = ShadowUiData.Shadow(
+                    0f, 0f, 20f, 10f, Color.parseColor("#999999")
+                ),
+                inner = ShadowUiData.Shadow(
+                    0f, 0f, 50f, -10f, Color.parseColor("#CCCCCC")
+                ),
+            ).setRound(
+                ShadowUiData.Round(
+                    leftTop = 30f,
+                    rightTop = 40f,
+                    rightBottom = 50f,
+                    leftBottom = 60f,
+                )
+            ).getUiData()
+        shadow.setData(ui)
     }
 
     inner class EventHandler : View.OnClickListener {
@@ -79,14 +50,7 @@ class HomeFragment : CommonFragment() {
             if (Once.beenDone(500L, "MainFragment.Click")) return
             Once.clearAndMarkDone("MainFragment.Click")
             when (v.id) {
-                R.id.btn_network -> {
-                    btn_network.visibility = View.GONE
-                    loadNetwork()
-                }
-                R.id.btn_local -> {
-                    btn_local.visibility = View.GONE
-                    loadLocal()
-                }
+
             }
         }
     }
--- modules/Plugin_Main/src/main/java/com/example/main/viewmodel/HomeViewModel.kt
+++ modules/Plugin_Main/src/main/java/com/example/main/viewmodel/HomeViewModel.kt
@@ -4,13 +4,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    val localTimer: MutableLiveData<Float> = MutableLiveData(0f)
-    val localStatus: MutableLiveData<LoadState> = MutableLiveData(LoadState.DEFAULT)
-
-    val networkTimer: MutableLiveData<Float> = MutableLiveData(0f)
-    val networkStatus: MutableLiveData<LoadState> = MutableLiveData(LoadState.DEFAULT)
-
-
     enum class LoadState(val event: String) {
         DEFAULT("未加载"), LOADING("加载中"), COMPLETE("加载成功"), FAIL("加载失败"),
     }
